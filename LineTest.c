27d4c63739d7d05d47b88e2d14e73afd45bab2cc
Elmuli <elmeri.elovaara@gmail.com>
2025-11-07 16:44:01 +0200

    Did more tweaking

--- LineTest.c
+++ LineTest.c
@@ -5,7 +5,7 @@
 #include <stdlib.h>
 #include <math.h>
  
-int widht = 600;
+int widht = 512;
 int hight = 400;
 SDL_Event windowEvent;
 
@@ -31,7 +31,8 @@ int main(int argc, char *argv[]) {
   SDL_FPoint Sinevalues[512];
   SDL_FPoint Sawvalues[512];
 
-  int waveType = 1;
+  int SineWave = 0;
+  int SawWave = 0;
 
   int current_sine_sample = 0;
   int current_saw_sample = 0;
@@ -59,16 +60,20 @@ int main(int argc, char *argv[]) {
         Amp -= 0.5f;
       }
       if(sc == SDL_SCANCODE_1){
-        waveType = 1;
+          SineWave = 1;
       }
       if(sc == SDL_SCANCODE_2){
-        waveType = 2;
+          SawWave = 1;
+      }
+      if(sc == SDL_SCANCODE_3){
+        SineWave = 0;
+        SawWave = 0;
       }
     }
     const int minimum_audio = (8000 * sizeof (float)) / 2;
     static float SineSamples[512];
     static float SawSamples[512];
-    if (SDL_GetAudioStreamQueued(stream) < minimum_audio && waveType == 1){
+    if (SDL_GetAudioStreamQueued(stream) < minimum_audio && SineWave){
       for (int i=0;i<=512;i++) {
         x = i*1;
         float phase = current_sine_sample*(Freq/8000.0f);
@@ -84,11 +89,11 @@ int main(int argc, char *argv[]) {
       current_sine_sample %= 8000;
     }
     
-    if (SDL_GetAudioStreamQueued(stream) < minimum_audio && waveType == 2){
+    if (SDL_GetAudioStreamQueued(stream) < minimum_audio && SawWave){
       for (int i=0;i<=512;i++){
         x = i*1;
         float phase = current_saw_sample*(Freq/8000.0f);
-        y = Amp*(phase*2-floor(phase*2));
+        y = Amp*(phase*1-floor(phase*1));
         SawSamples[i] = y;
         current_saw_sample++;
         SDL_FPoint xy;
@@ -101,10 +106,13 @@ int main(int argc, char *argv[]) {
     }
 
 
-    if (SDL_GetAudioStreamQueued(stream) < minimum_audio && waveType == 1){
+    if (SDL_GetAudioStreamQueued(stream) < minimum_audio){
+      if (SineWave){
       SDL_PutAudioStreamData(stream, SineSamples, sizeof (SineSamples));
-    }else if (SDL_GetAudioStreamQueued(stream) < minimum_audio && waveType == 2){
+      }
+      if(SawWave){
       SDL_PutAudioStreamData(stream, SawSamples, sizeof (SawSamples));
+      }
     }
 
     SDL_SetRenderScale(renderer, 1.0f,1.0f);
@@ -113,6 +121,7 @@ int main(int argc, char *argv[]) {
 
     SDL_SetRenderDrawColor(renderer,255,255,255,1);
     SDL_RenderLines(renderer, Sinevalues,  SDL_arraysize(Sinevalues));
+    SDL_SetRenderDrawColor(renderer,255,0,0,1);
     SDL_RenderLines(renderer, Sawvalues,  SDL_arraysize(Sawvalues));
 
     SDL_SetRenderDrawColor(renderer,255,255,255,255);
